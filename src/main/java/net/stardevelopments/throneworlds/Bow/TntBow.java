8da89325ef55eb8e0d735366fa0bee9bfd7ba6dc
SirGibbleFrits <sirgibblefrits@gmail.com>
2020-10-08 10:10:22 +1100

    bow
    bow

--- src/main/java/net/stardevelopments/throneworlds/Bow/TntBow.java
+++ src/main/java/net/stardevelopments/throneworlds/Bow/TntBow.java
@@ -1,14 +1,41 @@
 package net.stardevelopments.throneworlds.Bow;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.meta.ItemMeta;
+import java.util.ArrayList;
 
-public class TntBow{
+public class TntBow implements Listener {
 
     public ItemStack getTntBow(){
         ItemStack tntBow = new ItemStack(Material.BOW);
+        tntBow.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 999);
+
+        ItemMeta tntBowMeta = tntBow.getItemMeta();
+        tntBowMeta.setDisplayName("TNT Bow");
+
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add("Arrows detonate on impact");
+        tntBowMeta.setLore(lore);
+
         return tntBow;
+    }
+
+    @EventHandler
+    public void onShot(ProjectileHitEvent e){
+
+        if(e.getEntity() instanceof Player){
+
+            Player player = (Player) e.getEntity().getShooter();
+
+
+
+        }
+
     }
 }
