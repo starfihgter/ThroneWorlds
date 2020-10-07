b72aba9c803a2e18efbe79366d0f5426168f86e3
SirGibbleFrits <sirgibblefrits@gmail.com>
2020-10-08 09:28:13 +1100

    bow
    bow

---
+++ src/main/java/net/stardevelopments/throneworlds/Bow/TntBow.java
@@ -0,0 +1,14 @@
+package net.stardevelopments.throneworlds.Bow;
+
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.enchantments.Enchantment;
+
+public class TntBow implements Listener {
+
+    ItemStack tntbow = new ItemStack(Material.BOW);
+    tntbow.addEnchant
+
+
+}
