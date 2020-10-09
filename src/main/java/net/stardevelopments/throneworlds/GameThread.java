ec700539ec01fed84340d9f6f460019e80d050a4
starfihgter <alexi.rampono.kelly@gmail.com>
2020-10-09 22:24:37 +1100

    Portal generation
    probs not gonna stick with obsidian frames tho

--- src/main/java/net/stardevelopments/throneworlds/GameThread.java
+++ src/main/java/net/stardevelopments/throneworlds/GameThread.java
@@ -1,8 +1,12 @@
 package net.stardevelopments.throneworlds;
 
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
+import com.onarandombox.MultiverseCore.api.MultiverseWorld;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +14,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameThread implements CommandExecutor {
     Main plugin;
@@ -26,7 +31,50 @@ public class GameThread implements CommandExecutor {
 
     //Relocate Portals
     public void portalScatter(){
+        FileConfiguration teamsDB = Main.teamsDB.getUserRecord();
+        FileConfiguration worldState = Main.worldState.getUserRecord();
 
+        int totalTeams = plugin.getConfig().getInt("Teams", 4);
+        for (int i = 0; i < totalTeams; i++){
+
+            //Randomly generate x and z within play area
+            int radius = plugin.getConfig().getInt("border-radius");
+            int x = ThreadLocalRandom.current().nextInt(-radius, radius);
+            int z = ThreadLocalRandom.current().nextInt(-radius, radius);
+            teamsDB.set("team" + i + ".portal.x", x);
+            teamsDB.set("team" + i + ".portal.z", z);
+
+            //get y value of random coord
+            World overWorld = Bukkit.getWorld("Overworld");
+            Block yBlock = overWorld.getHighestBlockAt(x ,z);
+            int y = yBlock.getY();
+            teamsDB.set("team" + i + ".portal.y", y);
+
+            //Generate portal. I could not think of a better way to do this. Please feel free to change if I'm a moron
+            Material obsidian = Material.OBSIDIAN;
+            yBlock.setType(obsidian);
+            Block block;
+            for (int b = 1; b < 5; b++){
+                block = overWorld.getBlockAt(x, y + b, z);
+                block.setType(obsidian);
+            }
+            block = overWorld.getBlockAt(x + 1, y, z);
+            block.setType(obsidian);
+            block = overWorld.getBlockAt(x + 1, y, z);
+            block.setType(obsidian);
+            x = yBlock.getX();
+            y = yBlock.getX();
+            block = overWorld.getBlockAt(x + 1, y, z);
+            block.setType(obsidian);
+            block = overWorld.getBlockAt(x + 1, y, z);
+            block.setType(obsidian);
+            block = overWorld.getBlockAt(x + 1, y, z);
+            block.setType(obsidian);
+            for (int b = 1; b < 5; b++){
+                block = overWorld.getBlockAt(x, y + b, z);
+                block.setType(obsidian);
+            }
+        }
     }
 
     //Game Start
@@ -45,6 +93,9 @@ public class GameThread implements CommandExecutor {
 
         //Create overworld
         if(wm.cloneWorld("OverworldTemplate", "Overworld")){
+            World overWorld = Bukkit.getWorld("Overworld");
+            overWorld.getWorldBorder().setCenter(0,0);
+            overWorld.getWorldBorder().setSize(plugin.getConfig().getInt("border-radius"));
             out("Overworld created", sender);
 
             //Create Throne Worlds
