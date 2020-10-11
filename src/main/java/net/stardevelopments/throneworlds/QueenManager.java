1179cc98035d5b26b3c833116eba2706e8f979d7
starfihgter <alexi.rampono.kelly@gmail.com>
2020-10-11 18:08:04 +1100

    Outlining methods

--- src/main/java/net/stardevelopments/throneworlds/QueenManager.java
+++ src/main/java/net/stardevelopments/throneworlds/QueenManager.java
@@ -1,7 +1,20 @@
 package net.stardevelopments.throneworlds;
 
-public class QueenManager {
-    public void CreateQueens(){
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+public class QueenManager implements Listener {
+    //Game start
+    public void CreateQueens(){
+    }
+
+    //Queen UI
+    public void onRightClick(PlayerInteractEntityEvent e){
+    }
+
+    //Queen Death
+    public void onEntityDeath(EntityDeathEvent e){
+        //play epic music
     }
 }
